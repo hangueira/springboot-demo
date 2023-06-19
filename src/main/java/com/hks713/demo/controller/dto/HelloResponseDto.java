feb8ec3181a02448eaf9d4d073eae3222547cac6
hankyungsu <hks713@naver.com>
2023-06-19 23:11:44 +0900

    [ADDED] - Hello Controller, Dto, test case

---
+++ src/main/java/com/hks713/demo/controller/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.hks713.demo.controller.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
---
+++ src/test/java/com/hks713/demo/controller/dto/HelloResponseDtoTest.java
@@ -0,0 +1,26 @@
+package com.hks713.demo.controller.dto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class HelloResponseDtoTest {
+
+
+    @Test
+    void 롬복_기능_테스트() {
+        // given
+        String name = "Test";
+        int amount = 1000;
+
+        // when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        // then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+
+
+}
